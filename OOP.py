bdb0d789f073bd8bd30c61dfa504ff2504168997
divyd8314 <84455302+divyd8314@users.noreply.github.com>
2023-11-03 15:11:09 +0000

    updated

--- OOP.py
+++ OOP.py
@@ -19,3 +19,35 @@ print(orange_1.colour)
 
 orange_1.rot(3, 30)
 print(orange_1.mold)
+
+#Create a perimeter method, change size method, identify method 
+#Instantiate two new rectangles and change their size 
+#Run .perimeter on rec1 and .identify on rec2
+class Rectangle():
+    def __init__(self, w, l):
+        self.width = w
+        self.length = l
+    def area (self):
+        return self.width * self.length 
+    def perimeter (self):
+        return 2*self.width+ 2*self.length
+    def change_size (self, w, l):
+        self.width = w 
+        self.length = l 
+    def identify(self):
+        print("I am a rectangle")
+
+rec1 = Rectangle(3,4)
+print(rec1.area())
+
+rec2 = Rectangle(4, 5)
+rec3 = Rectangle(6, 7)
+rec3.change_size(4,10)
+print(rec2.perimeter())
+
+
+
+
+
+
+
