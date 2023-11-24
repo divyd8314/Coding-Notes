5600d8b345f9982e44dae46b4b104a776ae2525f
divyd8314 <84455302+divyd8314@users.noreply.github.com>
2023-11-24 16:15:16 +0000

    update

--- magic_methods.py
+++ magic_methods.py
@@ -19,3 +19,29 @@ class Car():
     
 car1 = Car('blue', 300000)
 print(car1)
+
+class AlwaysPositive:
+
+    def __init__(self,number):
+        self.n = number
+    
+    def __add__(self,other):
+        return abs(self.n + other.n)
+x = AlwaysPositive(-20)
+y = AlwaysPositive(10)
+
+print(x+y)
+#by affecting the __add_ method 
+#(-20+10)=-10, abs(-10) = 10
+#Therefore, 10 is the output 
+
+#Another example
+class Basket():
+    def __init__(self,contents):
+        self.contents = contents 
+    
+    def __add__ (self,other):
+        return Basket(self.contents + other.contents)
+
+mybasket = input("Enter basket items seperated by comma").split(',')
+basket = Basket (mybasket)
